3f4844781cfcbeb9bb4843d23741ce69dd3af4ab
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-02-20 11:11:46 +0200

    add components

---
+++ src/components/app/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from '../header';
+import Footer from '../footer'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Header />
+            <h3>This is App section</h3>
+        <Footer />
+    </div> 
+  
+  );
+}
+
+export default App;
